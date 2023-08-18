2f29dfa1cfeda2577146e59aaeed62a4ba03b128
CamiloOrozco-dev <orozcomugiwara@gmail.com>
2023-08-17 21:05:22 -0500

    Subimos cambios en los scripts

---
+++ JS/music-list.js
@@ -0,0 +1,30 @@
+const allMusic = [{
+
+        name: "HYAENA",
+        artist: "Travis Scott",
+        img: "Travis_Scott_Utopia",
+        url: "1 - HYAENA"
+
+    },
+    {
+        name: "MODERM JAM",
+        artist: "Travis Scott",
+        img: "Travis_Scott_Utopia",
+        src: "2 - MODERN JAM"
+
+    },
+    {
+        name: "MY EYES",
+        artist: "Travis Scott",
+        img: "Travis_Scott_Utopia",
+        url: "3 - MY EYES"
+
+    },
+    {
+        name: "THANK GOD",
+        artist: "Travis Scott",
+        img: "Travis_Scott_Utopia",
+        url: "4 - THANK GOD"
+
+    }
+]
